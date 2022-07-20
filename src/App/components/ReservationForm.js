8d2e19c2f41ad7e8038bb7fa2df36c62ff41db1f
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-07-20 10:21:29 -0600

    feat: add ReservationForm component

---
+++ src/App/components/ReservationForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class ReservationForm extends Component {
+  state = {
+      id: null,
+      name: "",
+      date: "",
+      time: "",
+      number: null
+  }
+
+  render() {
+    return (
+      <form>
+        <input 
+          type='text'
+          placeholder='Name'
+          name='name'
+          // value={this.state.name}
+          // onChange={event => this.handleChange(event)}
+        />
+        <input 
+          type='text'
+          placeholder='Date (mm/dd)'
+          name='date'
+          // value={this.state.date}
+          // onChange={event => this.handleChange(event)}
+        />
+        <input 
+          type='text'
+          placeholder='Time (0:00)'
+          name='time'
+          // value={this.state.time}
+          // onChange={event => this.handleChange(event)}
+        />
+        <input 
+          type='text'
+          placeholder='Number of Guests'
+          name='number'
+          // value={this.state.number}
+          // onChange={event => this.handleChange(event)}
+        />
+      </form>
+    )
+  }
+}
+
+
+export default ReservationForm;
